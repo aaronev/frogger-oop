39de5e7594ac6323cb8a6a596514f49e16e45e94
Aaron Villanueva <aarone.villanueva@gmail.com>
2017-10-20 09:45:44 -0700

    completes functionality, but will make it better

--- public/js/engine.js
+++ public/js/engine.js
@@ -9,9 +9,11 @@
  * drawn but that is not the case. What's really happening is the entire "scene"
  * is being drawn over and over, presenting the illusion of animation.
  *
- * This engine makes the canvas' context (ctx) object globally available to make 
- * writing app.js a little simpler to work with.
+ * This engine is available globally via the Engine variable and it also makes
+ * the canvas' context (ctx) object globally available to make writing app.js
+ * a little simpler to work with.
  */
+
 var Engine = (function(global) {
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
@@ -55,7 +57,7 @@ var Engine = (function(global) {
          * function again as soon as the browser is able to draw another frame.
          */
         win.requestAnimationFrame(main);
-    }
+    };
 
     /* This function does some initial setup that should only occur once,
      * particularly setting the lastTime variable that is required for the
@@ -78,21 +80,35 @@ var Engine = (function(global) {
      */
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        checkCollisions();
     }
 
-    /* This is called by the update function and loops through all of the
+    /* This is called by the update function  and loops through all of the
      * objects within your allEnemies array as defined in app.js and calls
      * their update() methods. It will then call the update function for your
      * player object. These update methods should focus purely on updating
-     * the data/properties related to the object. Do your drawing in your
+     * the data/properties related to  the object. Do your drawing in your
      * render methods.
      */
     function updateEntities(dt) {
         allEnemies.forEach(function(enemy) {
-            //enemy.update(dt);
+            enemy.update(dt);
         });
-        //player.update();
+
+        if ( Math.round(player.y / 80) === 0 ) {
+            player.update();
+        }
+
+        doc.getElementById('score').innerHTML = player.score;
+    }
+
+    function checkCollisions() {
+        allEnemies.forEach(function(enemy) {
+            if ( Math.round(enemy.x / 100) === Math.round(player.x / 100) && 
+                 Math.round(enemy.y / 80) === Math.round(player.y / 80) ) {
+                    player.reset();
+            }
+        });    
     }
 
     /* This function initially draws the "game level", it will then call
@@ -106,12 +122,12 @@ var Engine = (function(global) {
          * for that particular row of the game level.
          */
         var rowImages = [
-                '/images/water-block.png',   // Top row is water
-                '/images/stone-block.png',   // Row 1 of 3 of stone
-                '/images/stone-block.png',   // Row 2 of 3 of stone
-                '/images/stone-block.png',   // Row 3 of 3 of stone
-                '/images/grass-block.png',   // Row 1 of 2 of grass
-                '/images/grass-block.png'    // Row 2 of 2 of grass
+                'images/water-block.png',   // Top row is water
+                'images/stone-block.png',   // Row 1 of 3 of stone
+                'images/stone-block.png',   // Row 2 of 3 of stone
+                'images/stone-block.png',   // Row 3 of 3 of stone
+                'images/grass-block.png',   // Row 1 of 2 of grass
+                'images/grass-block.png'    // Row 2 of 2 of grass
             ],
             numRows = 6,
             numCols = 5,
@@ -122,22 +138,23 @@ var Engine = (function(global) {
          * portion of the "grid"
          */
         for (row = 0; row < numRows; row++) {
-          for (col = 0; col < numCols; col++) {
-              /* The drawImage function of the canvas' context element
-               * requires 3 parameters: the image to draw, the x coordinate
-               * to start drawing and the y coordinate to start drawing.
-               * We're using our Resources helpers to refer to our images
-               * so that we get the benefits of caching these images, since
-               * we're using them over and over.
-               */
-              ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
-          }
+            for (col = 0; col < numCols; col++) {
+                /* The drawImage function of the canvas' context element
+                 * requires 3 parameters: the image to draw, the x coordinate
+                 * to start drawing and the y coordinate to start drawing.
+                 * We're using our Resources helpers to refer to our images
+                 * so that we get the benefits of caching these images, since
+                 * we're using them over and over.
+                 */
+                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+            }
         }
+
         renderEntities();
     }
 
     /* This function is called by the render function and is called on each game
-     * tick. Its purpose is to then call the render functions you have defined
+     * tick. It's purpose is to then call the render functions you have defined
      * on your enemy and player entities within app.js
      */
     function renderEntities() {
@@ -148,7 +165,7 @@ var Engine = (function(global) {
             enemy.render();
         });
 
-        //player.render();
+        player.render();
     }
 
     /* This function does nothing but it could have been a good place to
@@ -164,16 +181,16 @@ var Engine = (function(global) {
      * all of these images are properly loaded our game will start.
      */
     Resources.load([
-        '/images/stone-block.png',
-        '/images/water-block.png',
-        '/images/grass-block.png',
-        '/images/enemy-bug.png',
-        '/images/char-boy.png'
+        'images/stone-block.png',
+        'images/water-block.png',
+        'images/grass-block.png',
+        'images/enemy-bug.png',
+        'images/char-boy.png'
     ]);
     Resources.onReady(init);
 
     /* Assign the canvas' context object to the global variable (the window
-     * object when run in a browser) so that developers can use it more easily
+     * object when run in a browser) so that developer's can use it more easily
      * from within their app.js files.
      */
     global.ctx = ctx;
